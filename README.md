34890afa51e9a3ab85bddcd68a7e3e951be0b4c6
HeHongbo <hehongbo@mail.com>
2021-10-21 09:32:34 +0800

    Code simplifications, drop mail composing feature and omitted port

--- README.md
+++ README.md
@@ -1,35 +1,63 @@
 # node-msmtp
 
+![npm version](https://img.shields.io/npm/v/node-msmtp)
+
 A simple msmtp wrapper for node. 
 
 ## Usage
 
-Create an object out of this library.
+- Install `node-msmtp`.
 
-```javascript
-const Msmtp = require("node-msmtp");
-let msmtp = new Msmtp({
-    mailAddress: "bob@example.com",
-    smtpHost: "smtp.example.com",
-    smtpPort: 465,
-    security: "tls",
-    username: "bob@example.com",
-    password: "yourpassword",
-    senderName: "Bob"
-});
-```
+  ```shell
+  npm install node-msmtp
+  ```
 
-Then call `sendMail()` method to send your mail.
+- Also, `msmtp` binary should exist on the system. You may want to install one with your package manager.
 
-```javascript
-msmtp.sendMail({
-    to: "alice@example.com",
-    recipientName: "Alice",
-    subject: "Greetings",
-    body: "It's a nice day!"
-}).then(() => {
-    console.log("Send successfully.");
-}).catch(e => {
-    console.log("Failed to send.");
-});
-```
+  ```shell
+  apt-get install msmtp # Debian/Ubuntu
+  apk add msmtp # Alpine Linux
+  brew install msmtp # Homebrew on macOS
+  ```
+
+- Create an object out of this library. This will spawn an `msmtp` process, which waits for your composed email.
+
+  ```javascript
+  const Msmtp = require("node-msmtp");
+  let msmtp = new Msmtp({
+      smtpHost: "smtp.example.com",
+      smtpPort: 465,
+      security: "tls",
+      username: "yourname@example.com",
+      password: "yourpassword",
+  });
+  ```
+  
+  To use STARTTLS, start the instance with `security: "starttls"`, or omit it if you want to use unencrypted SMTP.
+
+- Compose your mail and send with the `sendmail()` method. `msmtp` process will read recipients from the mail's header.
+
+  ```javascript
+  let mail = "";
+  
+  // Mail header starts here.
+  mail += "From: YourName <yourname@example.com>\r\n";
+  mail += "To: Someone <someone@example.com>\r\n";
+  mail += "Subject: Greetings\r\n";
+  mail += "\r\n";
+  // Mail body starts here.
+  mail += "It's a nice day!";
+  
+  msmtp.sendMail(mail).then(() => {
+      console.log("Send successfully.");
+  }).catch(e => {
+      console.log("Failed to send.");
+  });
+  ```
+
+  `msmtp` uses exit codes defined by `sysexits(3)`. Exit codes other than `0` will be thrown as a Javascript error.
+  
+  Typically, you will receive an `EX_NOHOST` if the host is not reachable (either a wrong input or a broken connection 
+  is possible), or `EX_NOPERM` if the username/password combination is not correct. 
+ 
+  See [sysexits](https://www.freebsd.org/cgi/man.cgi?query=sysexits&manpath=FreeBSD+4.3-RELEASE) for more information. 
--- node-msmtp.js
+++ node-msmtp.js
@@ -1,53 +1,23 @@
 const child_process = require("child_process");
 const sysexitsError = require("./sysexits-errors");
 
-const defaultPort = security => {
-    switch (security) {
-        case "tls":
-            return 465;
-        case "starttls":
-            return 587;
-        default:
-            return 25;
-    }
-}
-
-const mailHeader = (
-    {
-        to = "",
-        recipientName = "",
-        subject = "",
-        senderMail = "",
-        senderName = ""
-    }
-) =>
-    `From: ${
-        senderName !== "" ? `"${senderName}" <${senderMail}>` : `${senderMail}`
-    }\nTo: ${
-        recipientName !== "" ? `"${recipientName}" <${to}>` : `${to}`
-    }${
-        subject !== "" ? `\nSubject: ${subject}` : ""
-    }`;
-
 module.exports = class {
     constructor(
         {
-            mailAddress = "",
             smtpHost = "",
             smtpPort = "",
             security = "",
             username = "",
             password = "",
-            senderName = ""
         }
     ) {
-        if (smtpHost !== "" && mailAddress !== "" && password !== "") {
+        if (smtpHost !== "" && smtpPort !== "" && username !== "" && password !== "") {
             this.msmtpInstance = child_process.spawn("msmtp", [
                 `-v`,
                 `--host=${smtpHost}`,
-                `--port=${smtpPort || defaultPort(security)}`,
+                `--port=${smtpPort}`,
                 "--auth=on",
-                `--user=${username !== "" ? username : mailAddress}`,
+                `--user=${username}`,
                 `--passwordeval=printf ${password}`,
                 `--tls=${security === "tls" || security === "starttls" ? "on" : "off"}`,
                 `--tls-starttls=${security === "starttls" ? "on" : "off"}`,
@@ -59,21 +29,12 @@ module.exports = class {
                     throw new Error("MSMTP_NOT_INSTALLED");
                 } else throw err;
             });
-            this.senderName = senderName;
-            this.mailAddress = mailAddress;
         } else {
             throw new Error("MISSING_PARAM");
         }
     }
 
-    sendMail(
-        {
-            to = "",
-            recipientName = "",
-            subject = "",
-            body
-        }
-    ) {
+    sendMail(mail) {
         return new Promise((resolve, reject) => {
             this.msmtpInstance.on("close", code => {
                 if (code === 0) {
@@ -82,15 +43,7 @@ module.exports = class {
                     reject(sysexitsError(code));
                 }
             });
-            this.msmtpInstance.stdin.write(mailHeader({
-                to: to,
-                recipientName: recipientName,
-                subject: subject,
-                senderMail: this.mailAddress,
-                senderName: this.senderName
-            }));
-            this.msmtpInstance.stdin.write("\n\n");
-            this.msmtpInstance.stdin.write(body);
+            this.msmtpInstance.stdin.write(mail);
             this.msmtpInstance.stdin.end();
         });
     }
